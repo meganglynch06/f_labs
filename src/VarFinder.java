6ce4e2a37922348fbe03129bdb2a329ce95b5175
meganglynch06 <143025651+meganglynch06@users.noreply.github.com>
2023-12-13 22:56:28 -0500

    Final Submit VarFinder.java

--- src/VarFinder.java
+++ src/VarFinder.java
@@ -1,86 +1,198 @@
-import FinalProject.FastaSequence;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
-public class VarFinder 
+public class VarFinder
+
 {
-	// unfinished ideas: 
-	// GUI -> CSV file (Excel Sheet?)
-	// add way to shorten or clean up sequence header names
-	// add multi-threading method in case of large files
-	// add SNP method
-	// command line / GUI report of file name and location?
-	
-    public static void STRFinder(List<FastaSequence> sequences, int readingFrame, String outputFile) 
+		//encoutered some import issues so I incorporated the FastaSequence Parser methods into this class
+
+    private String header;
+    private String sequence;
+
+    public VarFinder(String header, String sequence) //constructor - no threads
+	{
+        	this.header = header;
+        	this.sequence = sequence;
+    	}
+
+    public synchronized static List<VarFinder> readFastaFile(String filepath) throws Exception
+	{
+        	List<VarFinder> HeaderAndSequences = new ArrayList<>();
+        	BufferedReader reader = new BufferedReader(new FileReader(filepath));
+
+	        String line;
+        	StringBuilder sequenceBuilder = new StringBuilder();
+        	String header = null;
+
+        	while ((line = reader.readLine()) != null)
+		{
+           		 if (line.startsWith(">"))
+			{
+               		 	if (header != null)
+				{
+                   			 VarFinder fastaSequence = new VarFinder(header, sequenceBuilder.toString());
+                   			 HeaderAndSequences.add(fastaSequence);
+        				 sequenceBuilder = new StringBuilder();
+     				}
+              			header = line.substring(1);
+           		}
+			 else
+			{
+               			sequenceBuilder.append(line);
+            		}
+        	}
+
+        	if (header != null && sequenceBuilder.length() > 0)
+		{
+           		 VarFinder fastaSequence = new VarFinder(header, sequenceBuilder.toString());
+           		 HeaderAndSequences.add(fastaSequence);
+        	}
+
+		reader.close();
+       		return HeaderAndSequences;
+	}
+	public String getHeader()
+	{
+        	return header;
+	}
+
+	public String getSequence()
+	{
+        	return sequence;
+    	}
+
+    public synchronized static void STRFinder(List<VarFinder> sequences, int readingFrame, String outputFile)
     {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) 
-        {
-            writer.write("Sequence Header, Start Position, Repeated Sequence, Repeat Count, Repeat Length\n\n");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile)))
+	{
+        	writer.write("Sequence Header, Start Position, Repeated Sequence, Repeat Count, Repeat Length\n");
 
-            for (FastaSequence sequence : sequences) 
-            {
-                String singleSequence = sequence.getSequence();
-                
-                if (readingFrame >= singleSequence.length()) 
-                {
-                    System.out.println("Reading frame exceeds sequence length for sequence: " + sequence.getHeader());
-                    continue; //skip too short
-                }
+            	for (VarFinder sequence : sequences)
+		{
+               		String singleSequence = sequence.getSequence();
 
-                for (int len = 2; len <= 6; len++) 
-                {
-                    for (int i = readingFrame; i <= singleSequence.length() - len; i += 3) 
-                    {
+                	if (readingFrame >= singleSequence.length())
+			{
+                    		System.out.println("Reading frame exceeds sequence length for sequence: " + sequence.getHeader());
+                   		 continue; // skip too short
+               		 }
+
+                for (int len = 2; len <= 6; len++)
+		{
+                    for (int i = readingFrame; i <= singleSequence.length() - len; i += 3)
+		    {
                         String sub = singleSequence.substring(i, i + len);
                         int x = i + len;
-                        while (x + len <= singleSequence.length() && singleSequence.substring(x, x + len).equals(sub)) 
-                        {
+
+                        while (x + len <= singleSequence.length() && singleSequence.substring(x, x + len).equals(sub))
+			{
                             x += len;
                         }
-                        if (x - i >= len * 2) {
-                            writer.write(sequence.getHeader() + "," + i + "," + sub + "," + (x - i) / len + "," + len + "\n");
+
+                        if (x - i >= len * 2)
+			{
+                            writer.write(sequence.getHeader() + "," + i + "," + sub + "," + (x - i) / len + "," + len + "\n\n");
                         }
                     }
                 }
             }
 
-            System.out.println("File Written");
-
-        } 
-        catch (IOException e) 
-        {
+        }
+	catch (IOException e)
+	{
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) 
-    {
-        try 
-        {
-            System.out.println("Enter File Pathway: ");
-            Scanner input = new Scanner(System.in);
-            String filePath = input.nextLine();
-            System.out.println("Enter the Reading Frame (0, 1, or 2): ");
-            int userReadingFrame = input.nextInt();
-            input.nextLine();
-            System.out.println("Enter Desired Output File Name: ");
-            String outputFile = input.nextLine();
+    public static int getMinSeqLength(List<VarFinder> sequences)
+	{
+    		int minSeqLen = Integer.MAX_VALUE;
 
-            List<FastaSequence> sequences = FastaSequence.readFastaFile(filePath);
+    		for (VarFinder sequence : sequences)
+		{
+        		int seqLen = sequence.getSequence().length();
 
-            
-            VarFinder.STRFinder(sequences, userReadingFrame, outputFile);
+        		if (seqLen < minSeqLen)
+			{
+           			 minSeqLen = seqLen;
+        		}
+    		}
 
-            input.close();
+    		return minSeqLen;
+	}
 
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace(); 
-        }
-    }
+   public synchronized static void SNPFinder(List<VarFinder> sequences, String outputFile)
+   {
+	try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, true)))
+	{
+	        writer.write("Potential Single Nucleotide Polymorphisms (SNPs)\n\n");
+
+	        int sequenceCount = sequences.size();
+		int minSequenceLength = getMinSeqLength(sequences);
+
+	        for (int i = 0; i < minSequenceLength; i++)
+		{
+	            char[] nucleotides = new char[4];
+	            int[] counts = new int[4];
+
+	            for (VarFinder sequence : sequences)
+		    {
+	                char currentNucleotide = sequence.getSequence().charAt(i);
+	                int index = "ACGT".indexOf(currentNucleotide);
+
+	                nucleotides[index] = currentNucleotide;
+	                counts[index]++;
+	            }
+
+	            for (int j = 0; j < 4; j++)
+		    {
+
+	                if (counts[j] >= sequenceCount * 0.99 && counts[j] <= sequenceCount * 1.01)
+									{
+	                 writer.write("Position: " + i + ", Nucleotide: " + nucleotides[j] + "\n");
+	            }
+	        }
+	    }
+
+	}
+	catch (IOException e)
+	{
+	        e.printStackTrace();
+	}
+   }
+
+   public static void main(String[] args)
+   {
+	try
+	{
+		System.out.println("Input File Pathway: ");
+		Scanner scanner = new Scanner(System.in);
+		String filePath = scanner.nextLine();
+		List<VarFinder> HeaderAndSequences = VarFinder.readFastaFile(filePath);
+
+		System.out.println("Enter the Reading Frame (0, 1, or 2): ");
+		int userReadingFrame = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Enter Desired Output File Name: ");
+		String outputFile = scanner.nextLine();
+
+		VarFinder.STRFinder(HeaderAndSequences, userReadingFrame, outputFile);
+		VarFinder.SNPFinder(HeaderAndSequences, outputFile);
+
+		System.out.println("File Written");
+
+		scanner.close();
+	}
+	catch (Exception e)
+	{
+		e.printStackTrace();
+	}
+   }
 }
+
